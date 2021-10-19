3b48e84f42a9fc8247b73d9f3dc733ce808ebaa5
ajaybarath <ajaybarath3@gmail.com>
2021-10-19 16:48:32 +0530

    Added: function to get the form data and store it in a class

--- script/formServices.js
+++ script/formServices.js
@@ -1,7 +1,30 @@
 const save = () => {
+    getFormData()
     alter("saveButton Clicked")
 }
 
 const resetForm = () => {
 
+}
+
+const getFormData = () => {
+    let name = document.querySelector("#name")
+    let address = document.querySelector("#address")
+    let state = document.querySelector("#state")
+    let zip = document.querySelector("#zip")
+    let city = document.querySelector("#city")
+    let phone = document.querySelector("#phone")
+    let id = getNewId()
+
+    let adderssBook = new AdderssBook(id, name, address, city, state, zip, phone);
+
+    console.log(adderssBook)
+
+}
+
+const getNewId = () => {
+    let contactId = localStorage.getItem("ContactId");
+    contactId = !contactId ? 1 : (contactId + 1);
+    localStorage.setItem("ContactId", contactId);
+    return contactId;
 }
