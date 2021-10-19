c9295b6c3798683b9486534d2923dc236394ca9e
ajaybarath <ajaybarath3@gmail.com>
2021-10-19 17:01:00 +0530

    Added: getters and setter and to string methods

--- script/AddressBook.js
+++ script/AddressBook.js
@@ -9,4 +9,73 @@ class AdderssBook {
         this.zip = params[5]
         this.phone = params[6]
     }
+
+    set id(id) {
+        this._id = id;
+    }
+
+    set name(name) {
+        let namrRegex = RegExp('^[A-Z]{1}[a-z]{2,}$')
+        if (namrRegex.test(name))
+            this._name = name;
+        else throw 'Name is Incorect'
+    }
+
+    set address(address) {
+        let addressRegex = RegExp("[a-zA-Z]{3,}[' '][a-zA-Z]{3,}[' '][a-zA-Z]{3,}")
+        if (addressRegex.test(address))
+            this._address = address;
+        else throw 'Address is invalid'
+    }
+
+    set city(city) {
+        this._city = city;
+    }
+
+    set state(state) {
+        this._state = state;
+    }
+
+    set zip(zip) {
+        this._zip = zip;
+    }
+
+    set phone(phone) {
+        let phoneRegex = RegExp("^([91][6-9][0-9]{9})$|^([+91][6-9][0-9]{9})$|^([6-9][0-9]{9})$")
+        if (phoneRegex.test(phone))
+            this._phone = phone;
+        else throw 'phone number is invalid'
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get address() {
+        return this._address;
+    }
+
+    get city() {
+        return this._city;
+    }
+
+    get state() {
+        return this._state;
+    }
+
+    get zip() {
+        return this._zip;
+    }
+
+    get phone() {
+        return this._phone;
+    }
+
+    toString() {
+        return "name= " + this.name + ", address= " + this.address + ", city= " + this.city + ", state= " + this.state + ", zip= " + this.zip + ", phone= " + this.phone;
+    }
 }
