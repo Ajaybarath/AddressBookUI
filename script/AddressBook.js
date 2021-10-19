4fa2265915b90d49ff6777435d6bd5548b876a22
ajaybarath <ajaybarath3@gmail.com>
2021-10-19 16:47:57 +0530

    Added: class to store the address book data

---
+++ script/AddressBook.js
@@ -0,0 +1,12 @@
+class AdderssBook {
+
+    constructor(params) {
+        this.id = params[0]
+        this.name = params[1]
+        this.address = params[2]
+        this.city = params[3]
+        this.state = params[4]
+        this.zip = params[5]
+        this.phone = params[6]
+    }
+}
