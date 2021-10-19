8c8c1df96ff901b155ab1499af02a7aec91d26f6
ajaybarath <ajaybarath3@gmail.com>
2021-10-19 17:44:22 +0530

    Added: function to save data to local storage

--- script/formServices.js
+++ script/formServices.js
@@ -1,30 +1,62 @@
+let contactListArr = new Array();
+let updatedContactObj;
+
 const save = () => {
-    getFormData()
-    alter("saveButton Clicked")
+    updatedContactObj = getFormData();
+    addToLocalStorage();
 }
 
 const resetForm = () => {
 
 }
 
+const addToLocalStorage = () => {
+    var contactList = JSON.parse(localStorage.getItem("ContactList"))
+    let contactLocalData;
+    if (contactList) {
+        contactLocalData = contactList.find(cntDta => cntDta._id === updatedContactObj._id);
+        if (!contactLocalData) {
+            contactList.push(updatedContactObj);
+        }
+        else {
+            const index = contactList.map(cntDta => cntDta._id).indexOf(contactLocalData._id);
+            contactList[index] = updatedContactObj;
+        }
+
+    }
+    else {
+        contactList = [updatedContactObj]
+    }
+
+    alert(updatedContactObj.toString())
+    localStorage.setItem("ContactList", JSON.stringify(contactList))
+    alert("saved successfullf")
+    resetForm();
+}
+
 const getFormData = () => {
-    let name = document.querySelector("#name")
-    let address = document.querySelector("#address")
-    let state = document.querySelector("#state")
-    let zip = document.querySelector("#zip")
-    let city = document.querySelector("#city")
-    let phone = document.querySelector("#phone")
+    let name = document.querySelector("#name").value;
+    let address = document.querySelector("#address").value;
+    let state = document.querySelector('#State').value;
+    let zip = document.querySelector("#zip").value;
+    let city = document.querySelector("#city").value;
+    let phone = document.querySelector("#phone").value;
     let id = getNewId()
 
-    let adderssBook = new AdderssBook(id, name, address, city, state, zip, phone);
-
-    console.log(adderssBook)
+    try {
+        let adderssBook = new AdderssBook(id, name, address, city, state, zip, phone);
+        console.log(adderssBook)
+        return adderssBook;
+    }
+    catch (e) {
+        alert(e)
+    }
 
 }
 
 const getNewId = () => {
     let contactId = localStorage.getItem("ContactId");
-    contactId = !contactId ? 1 : (contactId + 1);
+    contactId = !contactId ? 1 : (parseInt(contactId) + 1);
     localStorage.setItem("ContactId", contactId);
     return contactId;
 }
