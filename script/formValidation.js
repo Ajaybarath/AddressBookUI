fb17e6da409acce4d3ef73e23a39ff57925aad69
ajaybarath <ajaybarath3@gmail.com>
2021-10-19 16:30:08 +0530

    Added: validation for form field

---
+++ script/formValidation.js
@@ -0,0 +1,30 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+   
+    const name = document.querySelector('#name');
+    const nameError = document.querySelector('.name-error');
+    name.addEventListener('input', function () {
+        let nameRegex = RegExp("[A-Z][a-zA-Z]{2,}")
+        if (nameRegex.test(name.value))
+            nameError.textContent = "";
+        else nameError.textContent = "Name is incorrect";
+    })
+
+    const address = document.querySelector('#address');
+    const addressError = document.querySelector('.address-error');
+    address.addEventListener('input', function () {
+        let addressRegex = RegExp("[a-zA-Z]{3,}[' '][a-zA-Z]{3,}[' '][a-zA-Z]{3,}")
+        if (addressRegex.test(address.value))
+            addressError.textContent = "";
+        else addressError.textContent = "Address is invalid";
+    })
+
+    const phone = document.querySelector('#phone');
+    const phoneError = document.querySelector('.phone-error');
+    phone.addEventListener('input', function () {
+        let phoneRegex = RegExp("^([91][6-9][0-9]{9})$|^([+91][6-9][0-9]{9})$|^([6-9][0-9]{9})$")
+        if (phoneRegex.test(phone.value))
+         phoneError.textContent = "";
+        else phoneError.textContent = "phone is incorrect";
+    })
+
+})
